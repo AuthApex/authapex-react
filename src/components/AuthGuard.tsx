17130f3d8cab4bc531af1fc54545bc26df931f45
Radim Staněk <gtomyasek@gmail.com>
2025-09-20 19:31:09 +0200

    fix: make `messages` optional in `AuthGuard` and handle error state with retry logic

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from '@/hooks/useAuth';
 import { PropsWithChildren } from 'react';
-import { LoadingState, Typography } from 'gtomy-lib';
+import { ErrorState, LoadingState, Typography } from 'gtomy-lib';
 
 export interface AuthGuardMessages {
   userNotLoggedIn: string;
@@ -8,18 +8,45 @@ export interface AuthGuardMessages {
 }
 
 export interface AuthGuardProps extends PropsWithChildren {
-  messages: AuthGuardMessages;
+  messages?: AuthGuardMessages;
 }
 
 export function AuthGuard({ messages, children }: AuthGuardProps) {
-  const { isLoading, user } = useAuth();
+  const { isLoading, isError, user, error, refetch } = useAuth();
 
   if (isLoading) {
+    if (!messages) {
+      return null;
+    }
     return <LoadingState message={messages.loading} showLoading />;
   }
 
+  if (isError) {
+    if (!messages) {
+      return null;
+    }
+    // TODO: Add translation
+    return (
+      <ErrorState
+        error={error}
+        retry={refetch}
+        showRetry
+        translation={{
+          error: 'Error occured when loading user.',
+          retry: 'Try again',
+          badGateway: 'Server unreachable',
+          noPermission: "You don't have permission to view this.",
+        }}
+      />
+    );
+  }
+
   if (!user) {
+    if (!messages) {
+      return null;
+    }
     return <Typography as="div">{messages.userNotLoggedIn}</Typography>;
   }
+
   return children;
 }
